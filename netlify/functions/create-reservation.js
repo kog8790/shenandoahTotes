6beebaaa8ae5c4937ae5a30c19d78d7e40f38bdb
Kevin Green <kog8790@gmail.com>
2026-04-16 09:17:23 -0400

    Add create-reservation function for Airtable API

---
+++ netlify/functions/create-reservation.js
@@ -0,0 +1,23 @@
+// /netlify/functions/create-reservation.js
+export async function handler(event) {
+  const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
+  const BASE_ID = process.env.AIRTABLE_BASE_ID;
+
+  const data = JSON.parse(event.body);
+
+  const response = await fetch(`https://api.airtable.com/v0/${BASE_ID}/Reservations`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${AIRTABLE_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ fields: data })
+  });
+
+  const result = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
+}
